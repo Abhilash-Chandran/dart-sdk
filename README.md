7a412836012242e88c79796fe70f05ecc8fadbca
Abhilash Chandran <csc.abhilash@gmail.com>
2021-10-20 14:38:11 +0200

    Try to set up flutter based container.

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-#Dapr dart sdk
+# Dapr dart sdk
 
 Welcome to the dart-sdk for dapr. This repository aims to provide and maintain the dart packages required to interact with dapr sidecar.
-
---
+++ packages/dapr_proto/lib/dapr_proto.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dapr_proto;
+
+export 'src/dapr_proto_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
